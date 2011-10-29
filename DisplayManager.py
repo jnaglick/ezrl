4a27c8634646566eb74c0f93b49683ba2601793d
John M Naglick <john@netomat.net>
2011-10-29 19:33:46 -0400

    Simplified DisplayManager code: added putConsoleItem() which then calls putConsoleChar or putConsoleString as appropriate, turned the toConsoleItem helper-methods used by draw() in to just draw helpers (which was obviously the right thing to do), and renamed/cleaned up some of the code. Some credit goes to Matt, who's questions inspired some of these changes.

--- DisplayManager.py
+++ DisplayManager.py
@@ -4,71 +4,71 @@ from datetime import date
 
 class DisplayManager:
 	def __init__(self):
+		#console window width, height, title
 		self.C_WIDTH = 97
 		self.C_HEIGHT = 64
 		self.C_TITLE = 'New Game ' + date.today().strftime('%A %d-%B-%Y')
-		self.console = Console(self.C_WIDTH, self.C_HEIGHT, self.C_TITLE)
-		self.consoleItemGenerator = ConsoleItemGenerator()
+		
+		#map draw distances: how much of the map to draw around the player
+		self.M_HORZ_DRAW_DISTANCE = 45
+		self.M_VERT_DRAW_DISTANCE = 29
 
+		#offsets: where to draw stuff
 		self.infoOffset = (3, 1)
 		self.statusOffset = (3, 2)
 		self.mapOffset = (3, 4)
-		self.M_HORZ_DRAW_DISTANCE = 45
-		self.M_VERT_DRAW_DISTANCE = 29
 		
-	def _mapTileToConsoleItem(self, mapTile):
-		return self.consoleItemGenerator.createConsoleItem(mapTile)
-
-	def _playerToConsoleItem(self, player):
-		return ConsoleItem('@', white)
-
-	def _infoToConsoleItem(self, player):
-		s = 'X:[' + str(player.getX()) + '] Y:[' + str(player.getY()) + '] Steps:[' + str(player.getSteps()) + ']'
-		return ConsoleItem(s, Color(128, 128, 128))
+		self.console = Console(self.C_WIDTH, self.C_HEIGHT, self.C_TITLE)
+		self.mapTileToConsoleItem = MapTileToConsoleItem()
+			
+	def _drawPlayerInfo(self, player):
+		s = 'X:[' + str(player.getX()) + '] ' + \
+			'Y:[' + str(player.getY()) + '] ' + \
+			'Steps:[' + str(player.getSteps()) + ']'
+		c = ConsoleItem(s, Color(128, 128, 128))
+		self.console.putConsoleItem(self.infoOffset[0], self.infoOffset[1], c)
 		
-	def _statusToConsoleItem(self, player):
-		return ConsoleItem(player.getStatus(), Color(128, 128, 128))
+	def _drawPlayerStatus(self, player):
+		c = ConsoleItem(player.getStatus(), Color(128, 128, 128))
+		self.console.putConsoleItem(self.statusOffset[0], self.statusOffset[1], c)
 		
-	def draw(self, game):
-		player = game.getPlayer()
-		map = game.getMap()
-		
-		self.console.clear()
-
-		#draw info and status
-		self.console.putConsoleString(self.infoOffset[0], self.infoOffset[1], self._infoToConsoleItem(player))
-		self.console.putConsoleString(self.statusOffset[0], self.statusOffset[1], self._statusToConsoleItem(player))
-
-		#draw map
+	def _drawMap(self, map, x_start, x_stop, y_start, y_stop):
 		i = 0
-		for y in range(player.getY()-self.M_VERT_DRAW_DISTANCE, player.getY()+self.M_VERT_DRAW_DISTANCE+1):
+		for y in range(y_start, y_stop):
 			j = 0
-			for x in range(player.getX() - self.M_HORZ_DRAW_DISTANCE, player.getX() + self.M_HORZ_DRAW_DISTANCE+1):
-				consoleItem = self._mapTileToConsoleItem(map.getTile(x, y))
-				if consoleItem != None:
-					self.console.putConsoleChar(self.mapOffset[0] + j, self.mapOffset[1] + i, consoleItem)
+			for x in range(x_start, x_stop):
+				c = self.mapTileToConsoleItem.get(map.getTile(x, y))
+				if c != None:
+					self.console.putConsoleItem(self.mapOffset[0] + j, 
+												self.mapOffset[1] + i, 
+												c)
 				j += 1
 			i += 1
-
-		#draw player
-		self.console.putConsoleChar(self.mapOffset[0] + self.M_HORZ_DRAW_DISTANCE, \
-									self.mapOffset[1] + self.M_VERT_DRAW_DISTANCE, \
-									self._playerToConsoleItem(player))
 		
+	def _drawPlayer(self, player):
+		c = ConsoleItem('@', white)
+		self.console.putConsoleItem(self.mapOffset[0] + self.M_HORZ_DRAW_DISTANCE, \
+									self.mapOffset[1] + self.M_VERT_DRAW_DISTANCE, \
+									c)
+
+	def draw(self, game):
+		self.console.clear()
+		
+		player = game.getPlayer()
+
+		self._drawPlayerInfo(player)
+		self._drawPlayerStatus(player)
+		self._drawMap(game.getMap(), 
+					  player.getX() - self.M_HORZ_DRAW_DISTANCE, 
+					  player.getX() + self.M_HORZ_DRAW_DISTANCE+1,
+					  player.getY() - self.M_VERT_DRAW_DISTANCE, 
+					  player.getY() + self.M_VERT_DRAW_DISTANCE+1)
+		self._drawPlayer(player)
+
 		self.console.flush()
 
-class ConsoleItem:
-	def __init__(self, i, color, bcolor=None):
-		self.i = i
-		self.color = color
-		self.bcolor = bcolor
-		
-	def getI(self): return self.i
-	def getColor(self): return self.color
-	def getBColor(self): return self.bcolor
-	
-class ConsoleItemGenerator:
-	def createConsoleItem(self, mapTile):
+class MapTileToConsoleItem:
+	def get(self, mapTile):
 		consoleItem = None
 		if mapTile != None:
 			type = mapTile.getType()
@@ -83,22 +83,38 @@ class ConsoleItemGenerator:
 				elif mapTile.getProp('open') == 0: consoleItem = ConsoleItem('x', Color(188, 188, 188))
 		return consoleItem
 
+class ConsoleItem:
+	def __init__(self, i, color, bcolor=None):
+		self.i = i
+		self.color = color
+		self.bcolor = bcolor
+		
+	def getI(self): return self.i
+	def getColor(self): return self.color
+	def getBColor(self): return self.bcolor
+
 class Console:
 	def __init__(self, consoleWidth, consoleHeight, consoleTitle):
 		self.c = console_init_root(consoleWidth, consoleHeight, consoleTitle, False)
 		
-	def putConsoleChar(self, x, y, ConsoleItem):
-		console_set_char(self.c, x, y, ConsoleItem.getI())
-		console_set_fore(self.c, x, y, ConsoleItem.getColor())
-		if (ConsoleItem.getBColor() != None): console_set_back(self.c, x, y, ConsoleItem.getBColor())
+	def putConsoleItem(self, x, y, consoleItem):
+		if len(consoleItem.getI()) == 1:
+			self._putConsoleChar(x, y, consoleItem)
+		elif len(consoleItem.getI()) > 1:
+			self._putConsoleString(x, y, consoleItem)
 		
-	def putConsoleString(self, x, y, ConsoleItem):
-		console_print_left(self.c, x, y, BKGND_NONE, ConsoleItem.getI())
-		for i in range(0, len(ConsoleItem.getI())): 
-			console_set_fore(self.c, x+i, y, ConsoleItem.getColor())
-		if (ConsoleItem.getBColor() != None):
-			for i in range(0, len(ConsoleItem.getI())): 
-				console_set_back(self.c, x+i, y, ConsoleItem.getBColor())
+	def _putConsoleChar(self, x, y, consoleItem):
+		console_set_char(self.c, x, y, consoleItem.getI())
+		console_set_fore(self.c, x, y, consoleItem.getColor())
+		if (consoleItem.getBColor() != None): console_set_back(self.c, x, y, consoleItem.getBColor())
+		
+	def _putConsoleString(self, x, y, consoleItem):
+		console_print_left(self.c, x, y, BKGND_NONE, consoleItem.getI())
+		for i in range(0, len(consoleItem.getI())): 
+			console_set_fore(self.c, x+i, y, consoleItem.getColor())
+		if (consoleItem.getBColor() != None):
+			for i in range(0, len(consoleItem.getI())): 
+				console_set_back(self.c, x+i, y, consoleItem.getBColor())
 		
 	def flush(self):
 		console_flush()
